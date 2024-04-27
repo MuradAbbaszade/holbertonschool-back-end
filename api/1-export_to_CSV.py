fa389591626c32ce1f25ff68a57feb00125bf05a
root <muradabbaszade6@gmail.com>
2024-04-27 12:45:50 +0000

    Add task 2

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,27 +7,23 @@ import requests
 from sys import argv
 
 
-def main():
-    """
-    Main function
-    """
-    if len(argv) == 2:
-        url = "https://jsonplaceholder.typicode.com/"
-        user_url = "{}/users/{}".format(url, id)
-        todo_url = "{}/todos?userId={}".format(
-            url, id
-        )
+if len(argv) == 2:
+    url = "https://jsonplaceholder.typicode.com/"
+    user_url = "{}/users/{}".format(url, id)
+    todo_url = "{}/todos?userId={}".format(
+        url, id
+    )
 
-        user = requests.get(user_url).json()
-        todo_tasks = requests.get(todo_url).json()
-        csv2 = csv.writer(
-            open("{}.csv".format(int(argv[1])), "w"),
-            quoting=csv.QUOTE_ALL
+    user = requests.get(user_url).json()
+    todo_tasks = requests.get(todo_url).json()
+    csv2 = csv.writer(
+        open("{}.csv".format(int(argv[1])), "w"),
+        quoting=csv.QUOTE_ALL
+    )
+    for task in todo_tasks:
+        csv2.writerow(
+            [user["id"],
+             user["username"],
+             task["completed"],
+             task["title"]]
         )
-        for task in todo_tasks:
-            csv2.writerow(
-                [user["id"],
-                 user["username"],
-                 task["completed"],
-                 task["title"]]
-            )
